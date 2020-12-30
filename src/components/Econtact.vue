d314aac6dd1a87925d5d24eae89e2b9ec49f3522
NesaByte <nesabertanico@yahoo.com.ph>
2020-12-30 00:21:11 -0500

    Updated contact.vue: responsive page

--- src/components/Econtact.vue
+++ src/components/Econtact.vue
@@ -1,22 +1,21 @@
 <template>
-  <div
-    id="about"
-    class="about ssection"
-    data-aos="fade-up"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="true"
-  >
-    <h2>CONTACT US</h2>
-
-    <div class="center-box">
-      <div
-        class="text"
-        data-aos="fade-left"
-        data-aos-easing="ease-in-out"
-        data-aos-mirror="true"
-        data-aos-once="true"
-      >
+  <div id="contact" class="contact ssection">
+    <div
+      data-aos="fade-up"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="true"
+    >
+      <h2>POLICIES</h2>
+    </div>
+    <div
+      class="center-box container"
+      data-aos="fade-up"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="true"
+    >
+      <div class="text">
         <h1>Better yet, see us in person!</h1>
 
         <p>
@@ -26,12 +25,17 @@
           Our dental clinic offer you family environmental dental care. <br /><br />
         </p>
       </div>
-      <div class="row">
-        <div class="icons" data-aos="fade-down" data-aos-delay="150" data-aos-once="true">
+      <div class="row position align-items-stretch">
+        <div
+          class="portfolio-box-1"
+          data-aos="fade-up"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
           <div class="uvd-col">
             <div class="img">
               <img src="../assets/econtact1.png" alt class="img-fluid" />
-              <h4>Business Hours</h4>
+              <h3>Business Hours</h3>
             </div>
             <p>
               Monday: By appointment <br />
@@ -44,29 +48,44 @@
             </p>
           </div>
         </div>
-        <div class="icons" data-aos="fade-down" data-aos-delay="300" data-aos-once="true">
+        <div
+          class="portfolio-box-1"
+          data-aos="fade-up"
+          data-aos-delay="200"
+          data-aos-once="true"
+        >
           <div class="uvd-col">
             <div class="img">
               <img src="../assets/econtact2.png" alt class="img-fluid" />
-              <h4>Address</h4>
+              <h3>Address</h3>
             </div>
             <p>2 Station Lane, Unionville, ON, CA L3R 1R3</p>
           </div>
         </div>
-        <div class="icons" data-aos="fade-up" data-aos-delay="450" data-aos-once="true">
+        <div
+          class="portfolio-box-1"
+          data-aos="fade-up"
+          data-aos-delay="300"
+          data-aos-once="true"
+        >
           <div class="uvd-col">
             <div class="img">
               <img src="../assets/econtact3.png" alt class="img-fluid" />
-              <h4>Email</h4>
+              <h3>Email</h3>
             </div>
             <p>Dr Li's Email</p>
           </div>
         </div>
-        <div class="icons" data-aos="fade-up" data-aos-delay="600" data-aos-once="true">
+        <div
+          class="portfolio-box-1"
+          data-aos="fade-up"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
           <div class="uvd-col">
             <div class="img">
               <img src="../assets/econtact4.png" alt class="img-fluid" />
-              <h4>Phone</h4>
+              <h3>Phone</h3>
             </div>
             <p>(905) 604-9069</p>
           </div>
@@ -78,7 +97,7 @@
 
 <script>
 export default {
-  name: "Babout",
+  name: "Econtact",
   props: {
     msg: String,
   },
@@ -86,41 +105,29 @@ export default {
 </script>
 
 <style scoped>
-.about {
-  background: #e3e9d1;
+.contact {
+  background-color: #e3e9d1;
   background-size: cover;
   display: block;
 }
-hr.liner {
-  border: 1px solid#AE9C9C;
-  width: 85%;
-}
-.text {
-  display: block;
-  margin-top: 5%;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-.about h1 {
+.contact h1 {
   font-family: Tahoma;
   font-weight: 900;
   color: #707070;
   font-size: 15px;
-  margin-right: 35%;
-  margin-left: 40%;
+  text-align: center;
 }
-.about p {
+.contact p {
   font-family: Tahoma;
   color: #707070;
   font-size: 14px;
 }
 
-.uvd-col .img {
-  position: static;
-  vertical-align: middle;
-  border-style: none;
+.text {
+  display: block;
+  margin-top: 5%;
 }
-.img h4 {
+.img h3 {
   display: inline;
   text-align: left;
   padding: 0;
@@ -130,6 +137,15 @@ hr.liner {
   font-weight: 900;
   margin: 20px 1px 10px 10px;
 }
+.uvd-col li {
+  font-size: 14px;
+  line-height: 13px;
+  color: #333;
+  margin-bottom: 0;
+  padding: 0 20px 20px 20px;
+  word-wrap: break-word;
+}
+
 .uvd-col p {
   line-height: 13px;
   margin-bottom: 0;
@@ -139,6 +155,7 @@ hr.liner {
   color: #707070;
   font-size: 12px;
 }
+
 .uvd-col h3 {
   text-align: left;
   padding: 0;
@@ -148,154 +165,87 @@ hr.liner {
   font-size: 14px;
   font-weight: 900;
 }
-.icons {
-  box-sizing: border-box;
-  display: block;
-  flex-basis: 100%;
-  flex-grow: 0;
-  flex-shrink: 0;
-  font-size: 16px;
-  margin-left: 1%;
-  margin-right: 1%;
-  max-width: 23%;
-  width: 43%;
-  margin-bottom: 10px;
+
+hr.liner {
+  border: 1px solid#AE9C9C;
+  width: 85%;
 }
-.row {
-  display: block;
-  margin-top: 1%;
-  display: flex;
-  flex-wrap: wrap;
-  box-sizing: border-box;
-  width: 80%;
-  height: auto;
-  margin-right: 10%;
-  margin-left: 10%;
-}
+
 .img-fluid {
   max-width: 25%;
   max-height: 25%;
 }
 
-@media screen and (max-width: 2100px) {
+.uvd-col .img {
+  position: relative;
+  vertical-align: middle;
+  border-style: none;
 }
 
-@media screen and (max-width: 1600px) {
+.uvd-col {
+  border-radius: 0 0 4px 4px;
 }
 
-@media screen and (max-width: 1400px) {
+.row {
+  display: block;
+  margin-top: 5%;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  width: 100%;
+  height: auto;
+  right: 20%;
+  left: 20%;
 }
 
-@media screen and (max-width: 1200px) {
+.portfolio-box-1 {
+  box-sizing: border-box;
+  display: block;
+  flex-basis: 24%;
+  flex-grow: 0;
+  flex-shrink: 0;
+  font-size: 16px;
+  margin-left: 2%;
+  margin-right: 2%;
+  line-height: 24px;
+  max-width: 21%;
+  width: 240px;
+  margin-bottom: 50px;
 }
 
 @media screen and (max-width: 1024px) {
-  .text {
-    margin-top: 15%;
-    margin-left: 0%;
+  .portfolio-box-1 {
+    max-width: 46%;
+    flex-basis: 46%;
   }
 
-  .photo-box {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    margin-right: 0%;
+  .row {
+    width: 90%;
+    margin-right: 5%;
     margin-left: 5%;
-    margin-top: -5%;
-  }
-  .center-box h1 {
-    margin-left: 10%;
-    margin-right: 10%;
-    margin-top: 0%;
-    margin-bottom: 0%;
   }
 }
 
-@media screen and (max-width: 900px) {
-  .center-box h1 {
-    font-size: 18px;
+@media screen and (max-width: 512px) {
+  .portfolio-box-1 {
+    max-width: 96%;
+    flex-basis: 96%;
   }
-  .center-box h3 {
-    font-size: 30px;
+
+  .row {
+    width: 90%;
+    margin-right: 5%;
+    margin-left: 5%;
   }
 }
-
-@media screen and (max-width: 850px) {
-  .center-box h1 {
-    font-size: 18px;
-  }
-  .center-box h3 {
-    font-size: 29px;
-  }
-}
-
-@media screen and (max-width: 800px) {
-  .center-box h1 {
-    font-size: 17px;
-  }
-
-  .center-box h3 {
-    font-size: 28px;
-  }
-}
-
-@media screen and (max-width: 700px) {
-  .center-box h1 {
-    font-size: 16px;
-  }
-
-  .center-box h3 {
-    font-size: 26px;
-  }
-}
-
-@media screen and (max-width: 650px) {
-  .center-box h1 {
-    font-size: 15px;
-  }
-
-  .center-box h3 {
-    font-size: 23px;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .center-box h1 {
-    font-size: 15px;
-  }
-
-  .center-box h3 {
-    font-size: 23px;
-  }
+@media screen and (max-width: 750px) {
 }
 
 @media screen and (max-width: 550px) {
-  .center-box h1 {
-    font-size: 14px;
-  }
-  .center-box h3 {
-    font-size: 20px;
-  }
-}
-
-@media screen and (max-width: 500px) {
-  .center-box h1 {
-    font-size: 13px;
-  }
 }
 
 @media screen and (max-width: 450px) {
 }
-
-@media screen and (max-width: 400px) {
-  .center-box h1 {
-    font-size: 12px;
-  }
-}
-
 @media screen and (max-width: 350px) {
-  .center-box h1 {
-    margin-top: 20%;
-  }
 }
 </style>
